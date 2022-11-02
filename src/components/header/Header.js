a29ed75aa1e217f0cf623abf3f1f1aab4707a3e1
Aline Fernanda Bellozo <alinebellozo@gmail.com>
2022-11-01 22:52:50 -0700

    Files structuring

---
+++ src/components/header/Header.js
@@ -0,0 +1,35 @@
+// import "./header.scss";
+
+// function Header() {
+//   return (
+//     <header className="header">
+//       <span className="header__company-name">BrainStation 2.0</span>
+//       <nav className="header__navigation">
+//         <ul className="header__menu">
+//           <li className="header__menu-item">
+//             <a className="header__menu-item-link" href="#home">
+//               Home
+//             </a>
+//           </li>
+//           <li className="header__menu-item">
+//             <a className="header__menu-item-link" href="#about">
+//               About
+//             </a>
+//           </li>
+//           <li className="header__menu-item">
+//             <a className="header__menu-item-link" href="#courses">
+//               Courses
+//             </a>
+//           </li>
+//           <li className="header__menu-item">
+//             <a className="header__menu-item-link" href="#contactus">
+//               Contact Us
+//             </a>
+//           </li>
+//         </ul>
+//       </nav>
+//     </header>
+//   );
+// }
+
+// export default Header;
