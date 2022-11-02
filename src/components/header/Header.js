320564aa9009257f6f997449cbaeae336c3d110e
Aline Fernanda Bellozo <alinebellozo@gmail.com>
2022-11-02 15:27:17 -0700

    Project structuring

---
+++ src/components/header/Header.js
@@ -0,0 +1,23 @@
+import "./header.scss";
+
+function Header() {
+  return (
+    <header className="header">
+      <a href="/">
+        <img
+          src=""
+          alt="BrainFlix logo"
+          className="header__logo"
+        />
+      </a>
+      {/* <h1 className="header__text">
+        Bob's Burgers Family{" "}
+        <span role="img" aria-label="burger emoji">
+          🍔
+        </span>
+      </h1> */}
+    </header>
+  );
+}
+
+export default Header;
