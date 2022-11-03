0a7660d4679c6fa6931cb77c2bb682053371e913
Aline Fernanda Bellozo <alinebellozo@gmail.com>
2022-11-02 23:20:42 -0700

    Bug fixes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,7 +2,7 @@ import "./header.scss";
 import Logo from "../../assets/logo/Brainflix-logo.svg";
 import Image from "../../assets/images/Mohan-muruge.jpg";
 
-function Header(props) {
+export default function Header() {
   return (
     <header className="header">
         <div className="header__logo-container">
@@ -15,12 +15,12 @@ function Header(props) {
         </div>
         
         <nav className="header__nav-container">
-            <div className="header__nav-input-avatar">
-                <input className="header__input-field" placeholder="Search"></input>
+            <div className="header__nav-search-field-avatar">
+                <input className="header__search-field" placeholder="Search"></input>
                 <div className="header__avatar-container">
                     <img className="header__avatar"
                         src={Image}
-                        alt="Side of a man's face">
+                        alt="Left side of a man's face">
                     </img>
                 </div>
             </div>
@@ -30,5 +30,3 @@ function Header(props) {
     </header>
   );
 }
-
-export default Header;
